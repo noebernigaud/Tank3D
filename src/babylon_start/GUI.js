ce18a1d00b2e691f374fc64a6e39dce856d8c98a
noebernigaud <noe.bernigaud@etu.unice.fr>
2022-03-28 18:31:03 +0200

    Update GUI.js

--- src/babylon_start/GUI.js
+++ src/babylon_start/GUI.js
@@ -50,63 +50,6 @@ class Menu {
                 engine.runRenderLoop(() => scene.render())
             });
         })
-
-
-
-        // var button_bonus1 = BABYLON.GUI.Button.CreateSimpleButton("bonus1", "Bonus 1");
-        // button = button_bonus1;
-        // button_bonus1.width = "100px";
-        // button_bonus1.height = "40px";
-        // button_bonus1.color = "white";
-        // button_bonus1.background = "green";
-        // button_bonus1.paddingLeft = "5px";
-        // button_bonus1.paddingRight = "5px";
-        // panel.addControl(button_bonus1);
-
-        // var button_bonus2 = BABYLON.GUI.Button.CreateSimpleButton("bonus2", "Bonus 2");
-        // button2 = button_bonus2;
-        // button_bonus2.width = "100px";
-        // button_bonus2.height = "40px";
-        // button_bonus2.color = "white";
-        // button_bonus2.background = "green";
-        // button_bonus2.paddingLeft = "5px";
-        // button_bonus2.paddingRight = "5px";
-        // panel.addControl(button_bonus2);
-
-        // var button_bonus3 = BABYLON.GUI.Button.CreateSimpleButton("but2", "Bonus 3");
-        // button3 = button_bonus3;
-        // button_bonus3.width = "100px";
-        // button_bonus3.height = "40px";
-        // button_bonus3.color = "white";
-        // button_bonus3.background = "green";
-        // button_bonus3.paddingLeft = "5px";
-        // button_bonus3.paddingRight = "5px";
-        // panel.addControl(button_bonus3);
-
-        // button_bonus1.onPointerUpObservable.add(function () {
-        //     char1.speedNorme *= 2
-        //     button.dispose()
-        //     button2.dispose()
-        //     button3.dispose()
-        //     inMenu = false;
-        //     engine.runRenderLoop(() => scene.render())
-        // });
-        // button_bonus2.onPointerUpObservable.add(function () {
-        //     char1.speedNorme *= 2
-        //     button.dispose()
-        //     button2.dispose()
-        //     button3.dispose()
-        //     inMenu = false;
-        //     engine.runRenderLoop(() => scene.render())
-        // });
-        // button_bonus3.onPointerUpObservable.add(function () {
-        //     char1.speedNorme *= 2
-        //     button.dispose()
-        //     button2.dispose()
-        //     button3.dispose()
-        //     inMenu = false;
-        //     engine.runRenderLoop(() => scene.render())
-        // });
     }
 
 }
