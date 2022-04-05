46d77c81124dab1a99e59d45ae074d2fb1a14674
FissoreD <davide.fissore@etu.univ-cotedazur.fr>
2022-04-05 15:11:56 +0200

    Barre vie en fonction des PV

--- src/game_objects/healthBar.js
+++ src/game_objects/healthBar.js
@@ -14,9 +14,9 @@ class Healthbar {
     var healthBarContainerMaterial = new BABYLON.StandardMaterial("hb2mat", scene);
     var healthBarContainer = BABYLON.MeshBuilder.CreatePlane("hb2", { width: this.barWidth, height: 0.1, subdivisions: 4 }, scene);
 
-    healthBarContainerMaterial.diffuseColor = BABYLON.Color3.Gray();
-    healthBarContainerMaterial.emissiveColor = new BABYLON.Color3(1, 1, 1)
-    healthBarContainerMaterial.backFaceCulling = false;
+    healthBarContainerMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
+    healthBarContainerMaterial.emissiveColor = new BABYLON.Color4(0.08, 0.08, 0.08);
+    // healthBarContainerMaterial.backFaceCulling = false;
 
 
     healthBarContainer.position = new BABYLON.Vector3(0, 0.7, 0);     // Position above player.
@@ -26,23 +26,32 @@ class Healthbar {
     healthBarContainer.material = healthBarContainerMaterial;
     this.healthBarContainerMaterial = healthBarContainerMaterial;
     this.healthBarContainer = healthBarContainer;
-    this.partitions = 10;
+
+
+    // var healthBarTextMaterial = new BABYLON.StandardMaterial("hb3mat", scene);
+    // healthBarTextMaterial.diffuseColor = new BABYLON.Color3(20 / 255, 112 / 255, 25 / 255);
+
+
+    var healthBarTextMaterial = new BABYLON.StandardMaterial("hb3mat", scene);
+    healthBarTextMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
+    healthBarTextMaterial.emissiveColor = new BABYLON.Color3(20.0 / 255, 112.0 / 255, 25.0 / 255);
+    this.healthBarTextMaterial = healthBarTextMaterial;
   }
 
   updatePartition() {
     if (this.tank.type.name == ObjectEnum.Player.name) return;
+    this.partitions = this.tank.maxHealth;
+    let barsToDisplay = this.tank.health
     this.healthBarContainer.getChildMeshes().forEach(e => e.dispose())
-    var paddingBar = 0.02;
-    let barsToDisplay = Math.round(this.tank.health / this.tank.maxHealth * this.partitions)
+    var paddingBar = 0.01;
     let partitionWidth = (this.barWidth - (this.partitions + 1) * paddingBar) / this.partitions;
-    var healthBarTextMaterial = new BABYLON.StandardMaterial("hb3mat", scene);
-    healthBarTextMaterial.diffuseColor = BABYLON.Color3.Black();
 
     let createSmallBar = (offset, i) => {
       let xPos = offset * this.barWidth / 2 - offset * (i * (partitionWidth + paddingBar) + partitionWidth / 2) + paddingBar;
-      var healthBarText = BABYLON.MeshBuilder.CreatePlane("hb3", { width: partitionWidth, height: 0.08, subdivisions: 4, sideOrientation: BABYLON.Mesh.DOUBLESIDE }, scene);
+      console.log(xPos, xPos + paddingBar);
+      var healthBarText = BABYLON.MeshBuilder.CreatePlane("hb3", { width: partitionWidth, height: 0.07, subdivisions: 4, sideOrientation: BABYLON.Mesh.DOUBLESIDE }, scene);
       healthBarText.position = new BABYLON.Vector3(xPos, 0, offset * 0.01);
-      healthBarText.material = healthBarTextMaterial;
+      healthBarText.material = this.healthBarTextMaterial;
       healthBarText.parent = this.healthBarContainer
     }
 
