11816ed5e126a1fc6445bcfc16df945dc9e85781
noebernigaud <noe.bernigaud@etu.unice.fr>
2022-04-13 10:22:12 +0200

    wouhou
    resolu bug qui fait partir le laser et la balle du mauvais endroit en y

--- src/game_IA/shootAI.js
+++ src/game_IA/shootAI.js
@@ -1,6 +1,6 @@
 let f = (tank) => new BABYLON.Vector3(
     tank.shape.position.x + tank.getTurretTank().getDirection(BABYLON.Axis.Z).x * 7,
-    tank.shape.position.y + 3 / 40,
+    tank.shape.position.y + tank.getTurretTank().getDirection(BABYLON.Axis.Z).y * 7 + 0.1,
     tank.shape.position.z + tank.getTurretTank().getDirection(BABYLON.Axis.X).x * 7
 );
 
