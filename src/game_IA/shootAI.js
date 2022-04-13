0d7a52f52b89285b7ecac47ae1cd2fa897dac289
noebernigaud <noe.bernigaud@etu.unice.fr>
2022-04-13 10:23:15 +0200

    solve bug starting point bullet

--- src/game_IA/shootAI.js
+++ src/game_IA/shootAI.js
@@ -7,7 +7,7 @@ class ShootAI {
 
         return createRay(new BABYLON.Vector3(
             tank.shape.position.x + tank.getTurretTank().getDirection(BABYLON.Axis.Z).x * 5,
-            tank.shape.position.y + 3 / 40,
+            tank.shape.position.y + tank.getTurretTank().getDirection(BABYLON.Axis.Z).y * 7 + 0.1,
             tank.shape.position.z + tank.getTurretTank().getDirection(BABYLON.Axis.X).x * 5),
             tank.getTurretTank().getDirection(BABYLON.Axis.Z), 1000);
     }
